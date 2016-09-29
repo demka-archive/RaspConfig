8d0b4229fab0db66be4150d3e0f2a6fa92b98709
Georgiy Demenchuk <demenchuk.george@mail.ru>
2016-09-29 23:08:37 +0300

    OpenVPN for Centos7

--- docker.sh
+++ docker.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+#for CentOS 7
+yum check-update
+yum install epel-release -y
+yum install sudo curl wget nano nload htop -y
+wget -O screenfetch 'https://raw.github.com/KittyKatt/screenFetch/master/screenfetch-dev'
+chmod +x screenfetch
+sudo mv screenfetch /usr/bin/
+curl -fsSL https://get.docker.com/ | sh
+sudo systemctl start docker
 CID=$(docker run -d --restart=always --privileged -p 1194:1194/udp -p 443:443/tcp umputun/dockvpn)
 docker run -t -i -p 8080:8080 --volumes-from $CID umputun/dockvpn serveconfig
